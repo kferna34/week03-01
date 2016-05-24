8471317ffd2fe42d5edaf1c04aa21cc698b45ff5
kferna34 <kferna34@my.centennialcollege.ca>
2016-05-24 12:53:53 -0400

    added person class

---
+++ week03-01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+*Author: Krytia Fernandez
+*Date: may 24, 2016
+* version : 0.0.0.1 -- initial commit 
+
+*/
+namespace week03_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
